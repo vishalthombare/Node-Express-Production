8e5e80c56c4c12f4fd9798dac6cd6472ef09ebfb
vishalthombare <ivishal@outloo.in>
2021-01-29 17:48:56 +0530

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-const express = require('express')
-const app= express()
-const port=3004
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const port =3001
+
+const app = express();
+
+//use cors to allow cross origin resource sharing
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let brands = [];
+
+app.post('/create',(req, res)=> {
+  const newBook = {
+    Brand: req.body.brand,
+  };
+
+  brands.push(newBook);
+  console.log(brands);
+
+});
+
+app.get('/home', (req, res)=> {
+  console.log('Inside Home Login');
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+  console.log('Brands: ', JSON.stringify(brands));
+  res.end(JSON.stringify(brands));
+});
 
 
-app.get('/',(req, res)=>{
-    res.send('Hello Node Express')
-})
+app.listen(port, () => {
 
-app.listen(port, ()=>{
-    console.log(`Example app listening at http://localhost:${port}`)
-})  
+  console.log(`Sever is Start... http://localhost:${port}`)
+});
